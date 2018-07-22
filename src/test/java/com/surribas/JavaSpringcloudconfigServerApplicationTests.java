641e42b3f6f65231a43804ff84f842a4a547a411
Unknown <ricardo.surribas@gmail.com>
2018-07-22 18:02:39 -0300

    Version 0.0.1
    - First Commit

---
+++ src/test/java/com/surribas/JavaSpringcloudconfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.surribas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaSpringcloudconfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
