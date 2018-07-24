2292552d0e425fe58146556c446c1fefec1a4051
Unknown <ricardo.surribas@gmail.com>
2018-07-24 17:04:32 -0300

    Upload 0.0.3
    - Add Eureka Server

--- src/main/java/com/surribas/JavaSpringcloudconfigServerApplication.java
+++ src/main/java/com/surribas/JavaSpringcloudconfigServerApplication.java
@@ -3,9 +3,12 @@ package com.surribas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaServer
 public class JavaSpringcloudconfigServerApplication {
 
 	public static void main(String[] args) {
