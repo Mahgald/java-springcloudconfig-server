504666506497fc1af630c2bde801b98e6d44498a
Unknown <ricardo.surribas@gmail.com>
2018-07-25 08:10:17 -0300

    Test Eureka

--- src/main/java/com/surribas/JavaSpringcloudconfigServerApplication.java
+++ src/main/java/com/surribas/JavaSpringcloudconfigServerApplication.java
@@ -2,13 +2,12 @@ package com.surribas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class JavaSpringcloudconfigServerApplication {
 
 	public static void main(String[] args) {
